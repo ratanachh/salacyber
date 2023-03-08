71170d96fd5ecc9d6160970568380ee3ad8cc224
Ratana CHHORM <chhormratana@gmail.com>
2023-03-08 08:31:04 +0700

    update file

--- generate_pfx.sh
+++ generate_pfx.sh
@@ -1,1 +1,1 @@
-openssl pkcs12 -export -out aspnet/https/aspnetapp.pfx -inkey aspnet/letsencrypt/privkey.pem -in aspnet/letsencrypt/fullchain.pem -passin secureP@SSw0rdGCF078
+openssl pkcs12 -export -out aspnet/https/aspnetapp.pfx -inkey aspnet/letsencrypt/privkey.pem -in aspnet/letsencrypt/fullchain.pem -passin pass:secureP@SSw0rdGCF078
